f6569dec259d47aa2fd150f65e06a28feaef4e86
MercurialMune <munenecyp@gmail.com>
2019-01-23 18:13:37 +0300

    create a route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {PortfolioComponent } from './portfolio/portfolio.component';
+import {ProfileComponent} from './profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'profile', component: ProfileComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [PortfolioComponent, ProfileComponent]
